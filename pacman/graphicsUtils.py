30fc01e7ba46e0f751dcf502e3e91da3a74b5e06
MAlphaBecker <madeleine.becker@enta-paristech.fr>
2020-09-30 09:57:19 +0200

    Portage vers python3 : print()

--- pacman/graphicsUtils.py
+++ pacman/graphicsUtils.py
@@ -148,7 +148,7 @@ def end_graphics():
             if _root_window != None:
                 _root_window.destroy()
         except SystemExit, e:
-            print 'Ending graphics raised an exception:', e
+            print('Ending graphics raised an exception:', e)
     finally:
         _root_window = None
         _canvas = None
